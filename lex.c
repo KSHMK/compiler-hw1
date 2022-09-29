22357d3fc60bd48d919726cd91a1099f9d10310b
kshmk <ksh@enki.co.kr>
2022-09-29 15:56:55 +0900

    add lex list

---
+++ lex.c
@@ -0,0 +1,93 @@
+#include "lex.h"
+
+int check_1_9(char data)
+{
+    return '1' <= data && data <= '9';
+}
+
+int check_0(char data)
+{
+    return data == '0';
+}
+
+int check_0_9(char data)
+{
+    return check_0(data) || check_1_9(data);
+}
+
+int check_a_z(char data)
+{
+    return 'a' <= data && data <= 'z';
+}
+
+int check_A_Z(char data)
+{
+    return 'A' <= data && data <= 'Z';
+}
+
+int check_a_zA_Z(char data)
+{
+    return check_a_z(data) || check_A_Z(data);
+}
+
+int check_dot(char data)
+{
+    return data == '.';
+}
+
+int check_double_quote(char data)
+{
+    return data == '"';
+}
+
+int check_plus(char data)
+{
+    return data == '+';
+}
+
+int check_minus(char data)
+{
+    return data == '-';
+}
+
+int check_asterisk(char data)
+{
+    return data == '*';
+}
+
+int check_slash(char data)
+{
+    return data == '/';
+}
+
+int check_equal(char data)
+{
+    return data == '=';
+}
+
+int check_colon(char data)
+{
+    return data == ':';
+}
+
+int check_semicolon(char data)
+{
+    return data == ';';
+}
+
+int check_space(char data)
+{
+    return data == ' ';
+}
+
+int check_tab(char data)
+{
+    return data == '\t';
+}
+
+int check_enter(char data)
+{
+    return data == '\n';
+}
+
+
---
+++ lex.h
@@ -0,0 +1,23 @@
+#ifndef __LEX_H__
+#define __LEX_H__
+
+int check_1_9(char data);
+int check_0(char data);
+int check_0_9(char data);
+int check_a_z(char data);
+int check_A_Z(char data);
+int check_a_zA_Z(char data);
+int check_dot(char data);
+int check_double_quote(char data);
+int check_plus(char data);
+int check_minus(char data);
+int check_asterisk(char data);
+int check_slash(char data);
+int check_equal(char data);
+int check_colon(char data);
+int check_semicolon(char data);
+int check_space(char data);
+int check_tab(char data);
+int check_enter(char data);
+
+#endif
